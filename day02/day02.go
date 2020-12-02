3286ea600d1572880b7a01a51c6d8bc0a37923e3
rlchu <ryan.chupick@gmail.com>
2020-12-02 02:02:29 -0500

    day02 wip

---
+++ day02/day02.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type passwordLine struct {
+	low      int
+	high     int
+	letter   string
+	password string
+	count    int
+}
+
+// parseInput translates password input file into []passwordLine
+func parseInput(filename string) ([]passwordLine, error) {
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return []passwordLine{}, err
+	}
+	input := strings.Split(string(body), "\n")
+	ci := make([]passwordLine, len(input))
+	for i, val := range input {
+		s := strings.Split(val, " ")
+		ci[i].letter = string(s[1][0])
+		ci[i].password = s[2]
+
+		n := strings.Split(s[0], "-")
+		ci[i].low, err = strconv.Atoi(string(n[0]))
+		ci[i].high, err = strconv.Atoi(string(n[1]))
+	}
+	return ci, nil
+}
+
+func main2() {
+	passwordLine, _ := parseInput("day02_input.txt")
+	for k, line := range passwordLine {
+		passwordLine[k].count = 0
+		for i, passwordLetter := range line.password {
+			contLetterCount := 0
+			if line.letter == string(passwordLetter) {
+				for _, nextLetter := range []byte(line.password)[i:] {
+					if string(nextLetter) == line.letter {
+						contLetterCount++
+						continue
+					} else {
+						break
+					}
+				}
+			}
+			if contLetterCount >= passwordLine[k].count {
+				passwordLine[k].count = contLetterCount
+			}
+		}
+	}
+	finalCount := 0
+	for _, line := range passwordLine {
+		if line.low <= line.count && line.count <= line.high {
+			finalCount++
+		}
+	}
+	fmt.Println(finalCount)
+}
+
+func main() {
+	passwordLine, _ := parseInput("day02_input.txt")
+	// passwordLine, _ := parseInput("day02_example.txt")
+	for k, line := range passwordLine {
+		passwordLine[k].count = 0
+		for _, passwordLetter := range line.password {
+			if line.letter == string(passwordLetter) {
+				passwordLine[k].count++
+			}
+
+		}
+	}
+	finalCount := 0
+	for _, line := range passwordLine {
+		if line.low <= line.count && line.count <= line.high {
+			finalCount++
+		}
+	}
+	fmt.Println(finalCount)
+}
---
+++ day02/day02_test.go
@@ -0,0 +1,44 @@
+// package day02
+
+// import (
+// 	"testing"
+// )
+
+// type testCase struct {
+// 	fileName string
+// 	want     int
+// }
+
+// var testData01 = []testCase{
+// 	{"day01_example.txt", 514579},
+// 	{"day01_input.txt", 970816},
+// }
+
+// var testData02 = []testCase{
+// 	{"day01_example.txt", 241861950},
+// 	{"day01_input.txt", 96047280},
+// }
+
+// func TestPart1(t *testing.T) {
+// 	for _, val := range testData01 {
+// 		answer, err := Part1(val.fileName)
+// 		if err != nil {
+// 			t.Fatalf("%s", err)
+// 		}
+// 		if answer != val.want {
+// 			t.Fatalf("wanted: %v got: %v", val.want, answer)
+// 		}
+// 	}
+// }
+
+// func TestPart2(t *testing.T) {
+// 	for _, val := range testData02 {
+// 		answer, err := Part2(val.fileName)
+// 		if err != nil {
+// 			t.Fatalf("%s", err)
+// 		}
+// 		if answer != val.want {
+// 			t.Fatalf("wanted: %v got: %v", val.want, answer)
+// 		}
+// 	}
+// }
