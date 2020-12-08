f13f56cf2814a170d82f3f9f8e027076ce31c6bd
rlchu <ryan.chupick@gmail.com>
2020-12-08 01:30:37 -0500

    day07 wip

--- day07/day07.go
+++ day07/day07.go
@@ -15,11 +15,17 @@ func Part1(filename string) (int, error) {
 
 	// test := "dull black bags contain 2 shiny brown bags, 31 plaid bronze bags, 3 wavy teal bags, 3 dull chartreuse bag"
 
-	re := regexp.MustCompile(`\d+ \w+ \w+`)
+	re1 := regexp.MustCompile(`\w+ \w+`)
+	re2 := regexp.MustCompile(`\d+ \w+ \w+`)
+	line := make(map[string][]string)
 
-	for _, val := range re.FindAllString(groups[0], -1) {
-		fmt.Println(val)
+	subjectBag := re1.FindString(groups[0])
+
+	for _, val := range re2.FindAllString(groups[0], -1) {
+		line[subjectBag] = append(line[subjectBag], val)
 	}
+
+	fmt.Println(line)
 
 	return 0, nil
 }
