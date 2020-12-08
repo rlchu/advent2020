65266765db1d9309969b390297e3c81d6684e960
rlchu <ryan.chupick@gmail.com>
2020-12-08 01:21:19 -0500

    day07pt1 wip

--- day07/day07.go
+++ day07/day07.go
@@ -3,14 +3,23 @@ package day07
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
 	"strings"
 )
 
 // Part1 ...
 func Part1(filename string) (int, error) {
 	body, _ := ioutil.ReadFile(filename)
-	groups := strings.Split(string(body), "\n\n")
-	fmt.Println(groups)
+	groups := strings.Split(string(body), "\n")
+	fmt.Println(groups[0])
+
+	// test := "dull black bags contain 2 shiny brown bags, 31 plaid bronze bags, 3 wavy teal bags, 3 dull chartreuse bag"
+
+	re := regexp.MustCompile(`\d+ \w+ \w+`)
+
+	for _, val := range re.FindAllString(groups[0], -1) {
+		fmt.Println(val)
+	}
 
 	return 0, nil
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -1,13 +1,15 @@
 package day07
 
+import "testing"
+
 type testCase struct {
 	fileName string
 	want     int
 }
 
 var testData01 = []testCase{
-	{"day07_example.txt", 11},
-	{"day07_input.txt", 6714},
+	{"day07_example.txt", -1},
+	// {"day07_input.txt", 6714},
 }
 
 var testData02 = []testCase{
@@ -15,17 +17,17 @@ var testData02 = []testCase{
 	// {"day07_input.txt", 3435},
 }
 
-// func TestPart1(t *testing.T) {
-// 	for _, val := range testData01 {
-// 		answer, err := Part1(val.fileName)
-// 		if err != nil {
-// 			t.Fatalf("%s", err)
-// 		}
-// 		if answer != val.want {
-// 			t.Fatalf("wanted: %v got: %v", val.want, answer)
-// 		}
-// 	}
-// }
+func TestPart1(t *testing.T) {
+	for _, val := range testData01 {
+		answer, err := Part1(val.fileName)
+		if err != nil {
+			t.Fatalf("%s", err)
+		}
+		if answer != val.want {
+			t.Fatalf("wanted: %v got: %v", val.want, answer)
+		}
+	}
+}
 
 // func TestPart2(t *testing.T) {
 // 	for _, val := range testData02 {
